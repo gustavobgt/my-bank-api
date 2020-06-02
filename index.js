18a8abe4a801ffa920ec97354fcaac4169e56568
gustavobgt <gustavo_bgt97@hotmail.com>
2020-06-02 17:30:49 -0300

    Finalizando função de criação de usuário com post method, leitura e escrita de arquivo json com módulo fs

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 let express = require('express');
+let fs = require('fs');
+
 let app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(express.json());
+
+app.post('/account', (req, res) => {
+  let account = req.body;
+
+  fs.readFile('accounts.json', 'utf8', (err, data) => {
+    if (!err) {
+      try {
+        let json = JSON.parse(data);
+        account = { id: json.nextId++, ...account };
+        json.accounts.push(account);
+
+        fs.writeFile('accounts.json', JSON.stringify(json), (err) => {
+          if (err) {
+            res.status(400).send({ error: err.message });
+          } else {
+            res.end();
+          }
+        });
+      } catch (err) {
+        res.status(400).send({ error: err.message });
+      }
+    } else {
+      res.status(400).send({ error: err.message });
+    }
+  });
 });
 
 app.listen(3000, () => {
+  try {
+    fs.readFile('accounts.json', 'utf8', (err, data) => {
+      if (err) {
+        const initialJson = {
+          nextId: 1,
+          accounts: [],
+        };
+
+        fs.writeFile('accounts.json', JSON.stringify(initialJson), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
   console.log('API Started!');
 });
 
